10cea8de115dc7a2681e2075d0601b7ab2f196c7
Aldo Bertero <abertero@antica.cl>
2014-11-12 11:51:51 -0300

    added jsps and controller methods

--- src/main/java/portal/controllers/ApplicationController.java
+++ src/main/java/portal/controllers/ApplicationController.java
@@ -1,6 +1,7 @@
 package portal.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +13,30 @@ public class ApplicationController {
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView applicationIndex() {
         ModelAndView mv = new ModelAndView("home");
+        return mv;
+    }
+
+    @RequestMapping(value = "menu", method = RequestMethod.GET)
+    public ModelAndView mainMenu() {
+        ModelAndView mv = new ModelAndView("mainMenu");
+        return mv;
+    }
+
+    @RequestMapping(value = "garage/{altKeyGarage}", method = RequestMethod.GET)
+    public ModelAndView garageList(@PathVariable String altKeyGarage) {
+        ModelAndView mv = new ModelAndView("garageList");
+        return mv;
+    }
+
+    @RequestMapping(value = "order/{altKeyOrder}", method = RequestMethod.GET)
+    public ModelAndView saleOrder(@PathVariable String altKeyOrder) {
+        ModelAndView mv = new ModelAndView("saleOrder");
+        return mv;
+    }
+
+    @RequestMapping(value = "technician", method = RequestMethod.GET)
+    public ModelAndView technicianList() {
+        ModelAndView mv = new ModelAndView("technicianList");
         return mv;
     }
 }
