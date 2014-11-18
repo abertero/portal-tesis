70fe6b01c82630c1f0fa37fcac5089d8c8eb84d8
Aldo Bertero <abertero@antica.cl>
2014-11-18 08:10:07 -0300

    added log support

--- src/main/java/portal/config/ApplicationInitializer.java
+++ src/main/java/portal/config/ApplicationInitializer.java
@@ -4,6 +4,7 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.util.Log4jConfigListener;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -16,6 +17,8 @@ public class ApplicationInitializer implements WebApplicationInitializer {
 
     private static final String DISPATCHER_SERVLET_NAME = "dispatcher";
     private static final String DISPATCHER_SERVLET_MAPPING = "/";
+    private static final String LOG4J_CONFIG_LOCATION = "log4jConfigLocation";
+    private static final String LOG4J_CONFIG_LOCATION_VALUE = "/WEB-INF/spring/log4j.properties";
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
@@ -27,5 +30,9 @@ public class ApplicationInitializer implements WebApplicationInitializer {
         dispatcher.addMapping(DISPATCHER_SERVLET_MAPPING);
 
         servletContext.addListener(new ContextLoaderListener(rootContext));
+
+        servletContext.setInitParameter(LOG4J_CONFIG_LOCATION , LOG4J_CONFIG_LOCATION_VALUE);
+        Log4jConfigListener log4jListener = new Log4jConfigListener();
+        servletContext.addListener(log4jListener);
     }
 }
